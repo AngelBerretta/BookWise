71aa4ad971605668b19d8c3ab52c5c7ee28b27bc
Angel <angelcursodeingles2@gmail.com>
2026-07-17 22:01:59 -0300

    feat(toast): sistema de toasts con cola y stacking
    - ToastProvider global con cola de mensajes (sin taparse)
    - ToastCard con 4 variantes: success, error, warning, info
    - Sin dependencias externas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import { WishlistProvider } from './context/WishlistContext';
+import { ToastProvider } from './context/ToastContext';
 import AppRouter from './router/AppRouter';
 
 /**
  * Raíz de la aplicación.
  * Envuelve AppRouter con los contextos globales en el orden correcto:
  * AuthProvider primero (CartProvider y WishlistProvider dependen de él).
+ * ToastProvider afuera de todo: cualquier contexto (incluso Auth) podría
+ * necesitar mostrar un toast en el futuro (ej. sesión expirada).
  */
 const App = () => {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <WishlistProvider>
-          <AppRouter />
-        </WishlistProvider>
-      </CartProvider>
-    </AuthProvider>
+    <ToastProvider>
+      <AuthProvider>
+        <CartProvider>
+          <WishlistProvider>
+            <AppRouter />
+          </WishlistProvider>
+        </CartProvider>
+      </AuthProvider>
+    </ToastProvider>
   );
 };
 
