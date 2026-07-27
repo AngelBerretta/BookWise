c29a2ac7c451fcec7a9337ea5f622020977d8500
Angel <angelcursodeingles2@gmail.com>
2026-07-27 20:44:14 -0300

    fix(cart): centrar verticalmente el mensaje de carrito vacio en mobile

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -61,12 +61,16 @@ const Cart = () => {
   if (!products.length) {
     return (
       <div
-        className="flex flex-col justify-start sm:justify-center bg-[var(--bg)]"
+        className="flex flex-col bg-[var(--bg)]"
         style={{ minHeight: 'calc(100dvh - var(--navbar-h))' }}
       >
-        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-[clamp(1.5rem,8dvh,4rem)] pb-10 w-full">
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-[clamp(1.5rem,8dvh,4rem)] pb-10 w-full flex flex-col flex-1">
           <h1 className="h1-editorial mb-2">Mi carrito</h1>
-          <CartEmpty />
+          
+          {/* Nuevo contenedor que absorbe el espacio sobrante y centra su contenido */}
+          <div className="flex-1 flex flex-col justify-center">
+            <CartEmpty />
+          </div>
         </div>
       </div>
     );
